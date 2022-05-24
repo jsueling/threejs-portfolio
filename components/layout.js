62ef69fed66e31ae0acd683c755290daf9d16738
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-05-24 15:31:18 +0100

    started structuring

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,18 @@
+import Navbar from './navbar' // About - Projects
+import Footer from './footer' // Contact
+
+// https://stackoverflow.com/questions/68670753/why-is-nextjs-persistent-layout-re-rendering-all-the-time
+// https://nextjs.org/docs/basic-features/layouts
+
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return (
+  <>
+    <h1>Layout</h1>
+    <Navbar />
+    <main>
+      {children}
+    </main>
+    <Footer />
+  </>
+  )
 }
---
+++ pages/projects/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title>Projects</title>
+      </Head>
+      <h1>Projects</h1>
+    </>
+  )
+}
