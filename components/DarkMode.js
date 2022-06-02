03f90c26c5151e4bc8d6ba6ab156f6d322635ba5
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-02 17:37:05 +0100

    started darkMode class implementation

--- components/DarkMode.js
+++ components/DarkMode.js
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+// import useDarkMode from '../hooks/useDarkMode'
 
 import Sun from './Sun'
 import Moon from './Moon'
 
 export default function DarkMode() {
   const [dark, setDark] = useState('false')
+  // const
+
+  const changeDarkMode = () => {
+    setDark(!dark)
+  }
 
   return (
-    <button className='transition-all duration-[600ms] ease-in-out hover:rotate-[360deg]' onClick={() => setDark(!dark)}>
+    <button className='transition-all duration-[600ms] ease-in-out hover:rotate-[360deg]' onClick={changeDarkMode}>
       {dark ? <Sun /> : <Moon />}
     </button>
   )
--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ import Footer from './Footer' // Contact
 
 export default function Layout({ children }) {
   return (
-  <div className='sm:w-11/12 md:w-9/12 lg:w-7/12 m-auto'>
+  <div className='sm:w-11/12 md:w-9/12 lg:w-7/12 m-auto bg-white dark:bg-neutral-600'> {/* fix */}
     <h1>Layout</h1>
     <NavBar />
     <main>
