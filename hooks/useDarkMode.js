4843f105df860d823cb76771beadac62963c1e69
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-04 12:31:51 +0100

    fixed useDarkMode bugs

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react"
+import { useLayoutEffect, useEffect, useState } from "react"
 
 const useDarkMode = () => {
   const [dark, setDark] = useState()
-  useEffect(() => {
+
+  // on mount, check first in localStorage, else use prefers-color-scheme
+  useLayoutEffect(() => {
     const storedValue = JSON.parse(window.localStorage.getItem('darkMode'))
-    setDark(
-      typeof storedValue === Boolean
+    setDark( // enqueues changes to the component state https://reactjs.org/docs/react-component.html#setstate
+      typeof storedValue === 'boolean'
       ? storedValue
       : window.matchMedia('(prefers-color-scheme: dark)').matches // https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
     )
   }, [])
 
+  // when dark state changes (from the button onClick or from on mount useEffect => update localStorage + change body classList 
   useEffect(() => {
-    window.localStorage.setItem('darkMode', JSON.stringify(dark))
-    dark ? window.document.body.classList.add('dark') : window.document.body.classList.remove('dark')
+    if (dark !== undefined) {
+      window.localStorage.setItem('darkMode', JSON.stringify(dark))
+      dark ? window.document.body.classList.add('dark') : window.document.body.classList.remove('dark')
+    }
   }, [dark])
 
   return [dark, setDark]
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, // https://stackoverflow.com/a/61897567 strict mode rendering twice in dev
 }
 
 module.exports = nextConfig
