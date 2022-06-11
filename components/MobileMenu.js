0282ac023599d406517bec9b3eaabda181aec31e
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-11 14:21:32 +0100

    set up staggered animation for MobileMenu

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -2,12 +2,25 @@ import Link from 'next/link'
 
 export default function MobileMenu ({ menuItems, mobileMenuOpen, setMobileMenu }) {
 
+
+  // https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+  // https://github.com/tailwindlabs/tailwindcss/discussions/3461#discussioncomment-329183
+  // recommended to pre-create lookup table object instead of dynamically creating classNames on the client
+  const classMap = menuItems.reduce((prev, _, index) => {
+    return {
+      ...prev, [index]: `animate-[fadeInUp_.1s_ease-in-out_${index+1}s_1_normal_forwards] first:mt-5 mb-5 opacity-0`
+    }
+  }, {})
+
   return (
     <div className='flex flex-col items-start'>
-      {menuItems.map((item) =>
-        <Link key={item.title} href={item.href}>
-          <a onClick={() => setMobileMenu(!mobileMenuOpen)} className='first:mt-5 mb-5 border-2 border-green-600' >{item.title}</a>
-        </Link>
+      {menuItems.map((item, index) => {
+        return (
+          <Link key={item.title} href={item.href} >
+            <a onClick={() => setMobileMenu(!mobileMenuOpen)} className={classMap[index]}>{item.title}</a>
+          </Link>
+        )
+      }
       )}
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,9 @@ module.exports = {
     extend: {
       keyframes: {
         fadeInUp: {
-          'from': { opacity: '0', transform: 'translateY(10%)' },
-          'to': { opacity: '1', transform: 'translateY(0)' },
+          '0%': { opacity: '0', transform: 'translateY(20%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         }
-      },
-      animation: {
-        fadeInUp: 'fadeInUp 1s ease-in-out'
       },
       fontFamily: {
         'sans': ['Roboto',  ...defaultTheme.fontFamily.sans], // https://tailwindcss.com/docs/font-family#customizing-the-default-font
