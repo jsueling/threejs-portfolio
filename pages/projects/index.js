5f68fedc0a46bae3314b2f753fd160e45d67710f
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-14 14:59:00 +0100

    added fadeIn animation for page content

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,7 +6,12 @@ export default function Projects() {
       <Head>
         <title>Projects</title>
       </Head>
-      <h1>Projects</h1>
+      <div className='animate-fadeIn'>
+        <h1>Projects</h1>
+        <p>
+          Etiam diam quam, pharetra sit amet nibh sit amet, finibus mollis lorem. Vestibulum lobortis vestibulum placerat. Proin fringilla, nulla in imperdiet sollicitudin, eros purus gravida nisi, scelerisque egestas nunc tortor eu ante. Proin ornare ultrices arcu, eu dapibus enim imperdiet et. Mauris luctus turpis eget convallis mollis. Mauris molestie quam quis erat hendrerit, eget elementum ipsum porta. Vestibulum venenatis sagittis risus in ullamcorper. Vestibulum eget egestas libero. Integer a dolor a turpis fermentum efficitur. Vestibulum ultrices consectetur purus, ac ultricies libero posuere a.
+        </p>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,21 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        fadeInUp: {
+        fadeInBounce: {
           '0%': { opacity: '0', transform: 'translateY(20%)' },
           '70%': { opacity: '0.7', transform: 'translateY(-5%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(1%)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         }
       },
       fontFamily: {
         'sans': ['Roboto',  ...defaultTheme.fontFamily.sans], // https://tailwindcss.com/docs/font-family#customizing-the-default-font
+      },
+      animation: {
+        fadeIn: 'fadeInUp 0.5s ease-in-out 0s',
       }
     },
   },
