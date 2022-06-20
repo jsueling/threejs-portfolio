8b767367a861a96b4d489fae5701a316f3df3311
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-20 15:25:12 +0100

    added hooks directory to tailwind config https://tailwindcss.com/docs/content-configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./utils/**/*.js"
+    "./utils/**/*.js",
+    "./hooks/**/*.js"
   ],
   theme: {
     extend: {
