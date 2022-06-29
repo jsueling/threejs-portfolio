22cfb709c6dfccfc692aa1e6e71972514e81cdc0
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-29 12:11:52 +0100

    experimenting with animating position of grouped elements with useSpring

--- components/Spheres.js
+++ components/Spheres.js
@@ -7,16 +7,21 @@ import Sphere from './Sphere'
 export default function Spheres(props) {
   const numSpheres = 10
   const angle = (2 * Math.PI) / numSpheres
+
   const sphereArray = (new Array(numSpheres).fill()).map((_, i) => 
     [Math.cos(angle*i), Math.sin(angle*i), 0]
   )
 
   const group = useRef()
 
-  // const { magnitude } = useSpring({
-  //   magnitude: props.dark ? 10 : 2,
-  //   config: config.wobbly
-  // })
+  // https://gracious-keller-98ef35.netlify.app/docs/recipes/animating-with-react-spring/
+
+  const { spring } = useSpring({
+    spring: props.dark ? 0 : 1,
+    config: config.wobbly
+  })
+
+  const scale = spring.to([0, 1], [1, 5]);
 
   // goal is to orbit around a point
   useFrame((state, delta) => {
@@ -24,7 +29,7 @@ export default function Spheres(props) {
   })
 
   return (
-    <animated.group ref={group}>
+    <animated.group ref={group} scale-x={scale} scale-y={scale} scale-z={scale}>
       {sphereArray.map((v,i) =>
         <Sphere
           position={v}
