708d413050e4fbf52548fd134c356de3f624ac75
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-06-30 10:55:47 +0100

    fixed Sphere position animation

--- components/Sphere.js
+++ components/Sphere.js
@@ -1,14 +1,24 @@
 import { useRef } from 'react'
 
+import { useSpring, animated, config } from '@react-spring/three'
+
 export default function Sphere(props) {
   const mesh = useRef()
+  const expandedPosition = props.basePosition.map((v) => 10*v)
+
+  const { position } = useSpring({
+    position: props.dark ? props.basePosition : expandedPosition,
+    config: config.molasses
+  })
+
   return (
-    <mesh
+    <animated.mesh
       {...props}
+      position={position}
       ref={mesh}
       >
       <sphereGeometry args={[0.5, 32, 16]}/>
       <meshPhongMaterial color={'red'} />
-    </mesh>
+    </animated.mesh>
   )
 }
--- components/Spheres.js
+++ components/Spheres.js
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { useSpring, config, animated } from '@react-spring/three'
+import { animated } from '@react-spring/three'
 
 import Sphere from './Sphere'
 
@@ -8,7 +8,7 @@ export default function Spheres(props) {
   const numSpheres = 10
   const angle = (2 * Math.PI) / numSpheres
 
-  const sphereArray = (new Array(numSpheres).fill()).map((_, i) => 
+  const spherePositions = (new Array(numSpheres).fill()).map((_, i) => 
     [Math.cos(angle*i), Math.sin(angle*i), 0]
   )
 
@@ -16,24 +16,17 @@ export default function Spheres(props) {
 
   // https://gracious-keller-98ef35.netlify.app/docs/recipes/animating-with-react-spring/
 
-  const { spring } = useSpring({
-    spring: props.dark ? 0 : 1,
-    config: config.wobbly
-  })
-
-  const scale = spring.to([0, 1], [1, 5]);
-
-  // goal is to orbit around a point
   useFrame((state, delta) => {
     group.current.rotateOnAxis(props.axis, props.angle)
   })
 
   return (
-    <animated.group ref={group} scale-x={scale} scale-y={scale} scale-z={scale}>
-      {sphereArray.map((v,i) =>
+    <animated.group ref={group} >
+      {spherePositions.map((v,_) =>
         <Sphere
-          position={v}
-          key={i}
+          dark={props.dark}
+          basePosition={v}
+          key={v}
         />
       )}
     </animated.group>
