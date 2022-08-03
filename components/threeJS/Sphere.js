3143447238831f8ee628d407047823f737d9c715
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-08-03 12:38:56 +0100

    separated hover state from active state

--- components/threeJS/Sphere.js
+++ components/threeJS/Sphere.js
@@ -6,14 +6,24 @@ export default function Sphere(props) {
   const mesh = useRef()
   const [hovered, setHovered] = useState(false)
   const [active, setActive] = useState(false)
+  const [clicked, setClicked] = useState(false)
   
   useEffect(() => {
     document.body.style.cursor = hovered ? 'pointer' : 'auto'
   }, [hovered])
 
-  const handleHoverOut = debounce((e) => {
-    // TODO
-  })
+  const handlePointerIn = (e) => {
+    setHovered(true)
+    setActive(true)
+  }
+
+  const handlePointerOut = (e) => {
+    setHovered(false)
+    const test = debounce((e) => {
+      setActive(false)
+    }, 2000)
+    test()
+  }
 
   const activeFactor = active ? 1.5 : 1
   const darkFactor = props.dark ? 1 : 2
@@ -22,8 +32,8 @@ export default function Sphere(props) {
 
   const { color, position, scale, opacity } = useSpring({
     position: newPosition,
-    color: active ? 'blue' : hovered ? 'blue' : 'red',
-    scale: active ? 2 : hovered ? 2 : 1,
+    color: clicked ? 'blue' : active ? 'blue' : 'red',
+    scale: clicked ? 2 : active ? 2 : 1,
     opacity: props.dark ? 1.0 : 0.3,
     config: config.molasses
   })
@@ -36,9 +46,9 @@ export default function Sphere(props) {
       {...props}
       ref={mesh}
       position={position}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHovered(true)}
-      onPointerOut={(e) => setHovered(false)}
+      onClick={(e) => setClicked(!active)}
+      onPointerOver={handlePointerIn}
+      onPointerOut={handlePointerOut}
       scale={scale}
     >
       <sphereGeometry args={[0.5, 32, 16]} />
