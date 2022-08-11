d6016a9d713582ae455147c384bbdea8c984c13d
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-08-11 16:33:20 +0100

    fixed fadeOutStay

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,14 +18,18 @@ module.exports = {
         fadeInUp: {
           '0%': { opacity: '0', transform: 'translateY(1%)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
-        }
+        },
+        fadeOutDown: {
+          '100%': { opacity: '0', transform: 'translateY(0)' },
+          '0%': { opacity: '1', transform: 'translateY(-1%)' },
+        },
       },
       fontFamily: { // https://tailwindcss.com/docs/font-family#customizing-the-default-font
         'sans': ['Roboto',  ...defaultTheme.fontFamily.sans],
       },
       animation: { // duration | easing-function | delay | iteration-count | direction | fill-mode | play-state | name
         fadeInStay: '0.5s ease-in-out 0s 1 normal forwards running fadeInUp',
-        fadeOutStay: '0.5s ease-in-out 0s 1 reverse forwards running fadeInUp',
+        fadeOutStay: '0.5s ease-in-out 0s 1 normal forwards running fadeOutDown',
       },
       // https://coolors.co/fcf7ff-c4cad0-878c8f-a4969b-4e414a
       // https://coolors.co/ffffff-c4cad0-878c8f-6d5f64-43383f
