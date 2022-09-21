d09f60b524f77596cba653249f1488d64a917ebe
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-09-21 12:34:58 +0100

    added orbiting light

--- components/threeJS/Light.js
+++ components/threeJS/Light.js
@@ -8,24 +8,21 @@ export default function Light({ dark }) {
   const pointLight = useRef()
   const hemiLight = useRef()
   const orbitLight = useRef()
+  const group = useRef()
   useHelper(pointLight, PointLightHelper, 0.5, 'blue')
   useHelper(hemiLight, HemisphereLightHelper, 0.5, 'red')
   useHelper(orbitLight, PointLightHelper, 0.5, 'pink')
 
-  const angle = 0
-
-  const axis = new Vector3(0,0,1)
   useFrame((state, delta) => {
-    angle += 0.001
-    angle %= 2 * Math.PI
-    orbitLight.current.position = new Vector3(0,35*Math.sin(angle),35*Math.cos(angle))
+    group.current.rotation.x += 0.01
   })
 
   return (
     <>
-      <pointLight ref={orbitLight} position={[0,0,35]} intensity={4} />
       <hemisphereLight ref={hemiLight} color={'blue'} groundColor={'purple'} intensity={1} />
-      <pointLight ref={pointLight} intensity={dark ? 10 : 0.4} position={[1,1,1]} />
+      <group ref={group}>
+        <pointLight ref={orbitLight} position={[0,0,35]} intensity={1} />
+      </group>
     </>
   )
 }
