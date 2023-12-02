3bd8e6f3b5fa72e2451641fd893c44f61b23d7d1
James Sue-Ling <james_sueling@hotmail.co.uk>
2023-12-02 15:51:01 +0000

    bug fix basePath, could not load /scene.gtlf

--- components/threeJS/LowPolyEarth.js
+++ components/threeJS/LowPolyEarth.js
@@ -3,6 +3,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // thank you to: "LowPoly Earth" (https://skfb.ly/6TAUn) by JasperTobias is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 export default function LowPolyEarth(props) {
-  const gltf = useLoader(GLTFLoader, '/scene.gltf')
+  const gltf = useLoader(GLTFLoader, '/threejs-portfolio/scene.gltf')
   return <primitive {...props} object={gltf.scene} scale={0.1}/>
 }
