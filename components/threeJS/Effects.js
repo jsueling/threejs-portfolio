db3cb2580ab4dc36882dec1383602d211506f6ac
James Sue-Ling <james_sueling@hotmail.co.uk>
2024-02-05 13:15:06 +0000

    remove DepthOfField because of performance

--- components/threeJS/Effects.js
+++ components/threeJS/Effects.js
@@ -1,18 +1,15 @@
-import { EffectComposer, Bloom, Vignette, DepthOfField } from '@react-three/postprocessing'
+import { EffectComposer, Bloom, Vignette } from '@react-three/postprocessing'
 import { useFrame } from '@react-three/fiber';
 import { useState } from 'react';
 
 export default function Effects({ dark }) {
   const [intensity, setIntensity] = useState()
-  const [bokehScale, setBokehScale] = useState()
 
   const intensitySpeed = 0.00015
-  const bokehSpeed = 0.0001
 
   useFrame((_, delta) => {
     // Math.sin(Date.now() * Math.PI ) becomes 0 every millisecond following sin wave, bouncing between -1 and 1
     setIntensity(Math.sin(Date.now() * intensitySpeed * Math.PI) * 10 + 10)
-    setBokehScale(Math.sin(Date.now() * bokehSpeed * Math.PI) * 5 + 5)
   })
 
   return (
@@ -33,9 +30,6 @@ export default function Effects({ dark }) {
           eskil={true}
           offset={1.5}
           darkness={0.5}
-        />
-        <DepthOfField
-          bokehScale={bokehScale} // bokeh size
         />
       </>
       }
