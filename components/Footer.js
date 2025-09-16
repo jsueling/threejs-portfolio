c4a2e3ba389f075ed11e73578afb7c6ee4970098
James Sue-Ling <james_sueling@hotmail.co.uk>
2025-09-16 12:17:22 +0100

    updated repo/mail link

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 export default function Footer() {
   return (
     <div className='dark:text-white'>
-      <a href='https://github.com/jsueling' rel='noreferrer' target='_blank' className='pointer-events-auto h-6 w-6'>
+      <a href='https://github.com/jsueling/threejs-portfolio' rel='noreferrer' target='_blank' className='pointer-events-auto h-6 w-6'>
         <FontAwesomeIcon className='h-6 w-6' icon={faGithub} />
       </a>
-      <a href='mailto:james_sueling@hotmail.co.uk' rel='noreferrer' target='_blank' className='pointer-events-auto h-6 w-6 ml-4'>
+      <a href='mailto:j.sueling0@gmail.com' rel='noreferrer' target='_blank' className='pointer-events-auto h-6 w-6 ml-4'>
         <FontAwesomeIcon className='h-6 w-6' icon={faEnvelope} />
       </a>
     </div>
